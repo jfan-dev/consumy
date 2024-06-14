a45723a83debc98b2b37bcf269ec6ac2000b9fff
jfan-dev <jfan.dev+github@gmail.com>
2024-06-14 13:57:51 -0300

    :floppy_disk: Create local storage utility classes

--- src/storage.ts
+++ src/storage.ts
@@ -1,17 +1,37 @@
-function store(key: string, value: string) {
-  localStorage.setItem(key, value);
+class PersistentStorage implements SimpleStorage {
+  store(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  get(key: string) {
+    return localStorage.getItem(key);
+  }
+
+  remove(key: string) {
+    localStorage.removeItem(key);
+  }
 }
 
-function get(key: string): string | null {
-  return localStorage.getItem(key);
+class TransientStorage implements SimpleStorage {
+  store(key: string, value: string) {
+    sessionStorage.setItem(key, value);
+  }
+
+  get(key: string) {
+    return sessionStorage.getItem(key);
+  }
+
+  remove(key: string) {
+    sessionStorage.removeItem(key);
+  }
 }
 
-function remove(key: string) {
-  localStorage.removeItem(key);
+function createStorage(persistent: boolean): SimpleStorage {
+  return persistent ? new PersistentStorage() : new TransientStorage();
 }
 
-export const storage = {
-  store,
-  get,
-  remove
-};
+interface SimpleStorage {
+  store(key: string, value: string): void;
+  get(key: string): string | null;
+  remove(key: string): void;
+}
