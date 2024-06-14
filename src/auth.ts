74da2f581ef3a81037055846f094c2cf5bc031c8
jfan-dev <jfan.dev+github@gmail.com>
2024-06-13 23:15:46 -0300

    :bento: Enhance authentication utility with local storage and error handling

--- src/auth.ts
+++ src/auth.ts
@@ -1,24 +1,65 @@
-async function signIn(email, password, onSuccess) {
-  console.log("will sign in...")
-  const body = {
-  login: {
-  email: email,
-  password: password
-  }
-  }
-  const response = await fetch(
-  "http://localhost:3000/sign_in", {
-  method: "POST",
-  headers: {
-  "Accept": "application/json",
-  "Content-Type": "application/json"
-  },
-  body: JSON.stringify(body)
-  })
-  response.json().then((json) => {console.log(json); onSuccess()})
- }
+import { storage } from './storage.ts';
 
+async function signIn(email: string, password: string, onSuccess: () => void, onFailure: () => void) {
+  const body = {
+    login: {
+      email: email,
+      password: password
+    }
+  };
+
+  const response = await fetch("http://localhost:3000/sign_in", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
+  });
+
+  if (response.ok) {
+    success(response, onSuccess);
+  } else {
+    failure(response, onFailure);
+  }
+}
+
+function success(response: Response, onSuccess: () => void) {
+  response.json().then((json) => {
+    storage.store('token', json.token);
+    storage.store('email', json.email);
+    onSuccess();
+  });
+}
+
+function failure(response: Response, onFailure: () => void) {
+  onFailure();
+}
+
+function loggedIn(): boolean {
+  return Boolean(storage.get('token'));
+}
+
+function signOut(andThen: () => void = null) {
+  storage.remove('token');
+  storage.remove('email');
+  if (typeof andThen === 'function') {
+    andThen();
+  }
+}
+
+function currentUser() {
+  if (!loggedIn()) {
+    return null;
+  }
+  return {
+    email: storage.get('email')
+  };
+}
 
 export const auth = {
-  signIn: signIn
- }
+  signIn,
+  loggedIn,
+  currentUser,
+  signOut
+};
