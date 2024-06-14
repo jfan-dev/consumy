e464c5134b4a2d7f4bae3438deb54bd0e5bf8543
jfan-dev <jfan.dev+github@gmail.com>
2024-06-13 22:26:09 -0300

    :sparkles: Create Welcome view with navigation to SignIn page

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1>Delivery: Welcome</h1>
+    <nav>
+      <RouterLink :to="{ name: 'signin' }">Sign In</RouterLink>
+    </nav>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
