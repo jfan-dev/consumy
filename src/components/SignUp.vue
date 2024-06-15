574634597f8c5d4983f3bd04b56b07bea7fdc69e
jfan-dev <jfan.dev+github@gmail.com>
2024-06-15 17:50:03 -0300

    :lipstick: General UI and Style update

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -24,23 +24,191 @@ function onSubmit() {
 </script>
 
 <template>
-  <div>
-    <h1>Sign Up</h1>
-    <form @submit.prevent="onSubmit">
-    
-      <label>E-Mail: </label>
-      <input v-model="email" type="email"><br />
-    
-      <label>Senha: </label>
-      <input v-model="password" type="password"><br />
 
-      <label>Confirme a senha: </label>
-      <input v-model="password_confirmation" type="password"><br />
-    
-      <label>Remember Me: </label>
-      <input v-model="remember" type="checkbox"><br />
+  <div class="register-container flex">
 
-      <button type="submit" v-show="!awaiting">Sign Up</button>
-    </form>
+    <div class="register-image">
+        <img src="../assets/imgs/login-img1.png" alt="Godzilla eating">
+        <div class="testimonial flex">
+          <img src="https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2022/12/hamburguer-unsplash.jpg?w=1200&h=1200&crop=1" alt="Dish" class="dish-image">
+          <div class="testimonial-content">
+              <div class="stars">★★★★★</div>
+              <p>"O entregador chegou e a energia caiu, depois voltou até mais forte, de vez em quando a imagem do pc até fica azul radioativo rsrs... e o hamburguer é de fato irresistível!"</p>
+          </div>
+      </div>
+    </div>
+
+    <div class="register-form">
+        <h2>Crie sua Conta</h2>
+
+        <form @submit.prevent="onSubmit">
+          <label for="fullname">Nome Completo</label>
+          <input v-model="name" type="text" id="fullname" name="fullname" required>
+
+          <label for="email">Email</label>
+          <input v-model="email" type="email" id="email" name="email" required>
+
+          <label for="username">Usuário</label>
+          <input type="text" id="username" name="username" required>
+
+          <label for="password">Senha</label>
+          <input v-model="password" type="password" name="password" required>
+          
+          <label for="password">Confirme a senha</label>
+          <input v-model="password_confirmation" type="password" name="password" required>
+          
+          <div class="remember">
+            <input v-model="remember" type="checkbox"><br />
+            <label>Lembrar minha senha </label>
+          </div>
+
+          <button type="submit" v-show="!awaiting">Cadastrar</button>
+          <p class="login">Já tem uma conta? <a href="/signin">Faça login</a></p>
+      </form>
+    </div>
+
   </div>
 </template>
+
+<style>
+.register-container {
+  background: white;
+  display: flex;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.register-image, .register-form {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.register-image img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.testimonial {
+  position: absolute;
+  background-color: rgba(255, 255, 255, 0.89);
+  padding: 20px;
+  border-radius: 10px;
+  text-align: center;
+  max-width: 400px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  margin-top: 500px;
+}
+
+.stars {
+  color: #ffd700;
+  font-size: 1.5em;
+}
+
+.testimonial-content p {
+  font-size: 1em;
+  color: #666;
+  margin: 10px;
+}
+
+.dish-image {
+  width: 80px !important;
+  height: 100px !important;
+  border-radius: 10px;
+  margin: auto;
+}
+
+.register-form {
+  height: 100vh;
+  flex: 1;
+  flex-direction: column;
+  padding: 40px;
+}
+
+.register-form h2 {
+  margin-bottom: 20px;
+  font-size: 2em;
+  color: #333;
+}
+
+.register-form form {
+  width: 100%;
+  max-width: 400px;
+}
+
+.register-form label {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: bold;
+  color: #555;
+}
+
+.rememberMe  > * {
+  display: inline-block !important;
+  width: inherit !important;
+  margin-right: 5px;
+}
+
+.register-form input {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.register-form button {
+  width: 100%;
+  padding: 10px;
+  background-color: var(--red);
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  font-size: 1em;
+  cursor: pointer;
+}
+
+.register-form button:hover {
+  background-color: var(--orange);
+}
+
+.login {
+  margin-top: 20px;
+  text-align: center;
+}
+
+.login a {
+  color: var(--red);
+  text-decoration: none;
+}
+
+.login a:hover {
+  text-decoration: underline;
+}
+
+.remember {
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  font-weight: 300;
+  margin-bottom: 10px
+}
+
+.remember label {
+  margin-bottom: 0;
+}
+
+.remember input[type='checkbox'] {
+  width: 15px;
+  margin-bottom: 1px;
+  margin-right: 8px;
+}
+
+@media (max-width: 650px) {
+  .register-image {
+    display: none;
+  }
+}
+
+</style>
