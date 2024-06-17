a32a96fba4d9423960e0c785855a82d822377f94
jfan-dev <jfan.dev+github@gmail.com>
2024-06-17 07:30:50 -0300

    ✨ feat(router): add route for StoresList component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WelcomeView from '../views/WelcomeView.vue'
-import SignIn from '../components/SignIn.vue'
-import SignUp from '../components/SignUp.vue'
+import WelcomeView from '@/views/WelcomeView.vue'
+import SignIn from '@/components/SignIn.vue'
+import SignUp from '@/components/SignUp.vue'
+import StoresList from '@/components/StoresList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'welcome',
-      component: WelcomeView
-    },
-
-    {
-      path: '/signin',
-      name: 'signin',
-      component: SignIn
-    },
-
-    {
-      path: '/signup',
-      name: 'signup',
-      component: SignUp
-    }
+    { path: '/', name: 'welcome', component: WelcomeView },
+    { path: '/signin', name: 'signin', component: SignIn },
+    { path: '/signup', name: 'signup', component: SignUp },
+    { path: '/stores', name: 'stores', component: StoresList }
   ]
 })
 
